b29a841082d1f32f067f7d6828a80e7469c7fd84
haiducc <nguyenhaiduc2506@gmail.com>
2024-10-18 14:44:03 +0700

    commit helper test

--- app/src/helper/mailer.ts
+++ app/src/helper/mailer.ts
@@ -1,46 +1,46 @@
-import nodemailer from 'nodemailer'
-import User from '@/models/userModels'
-import bcryptjs from 'bcryptjs'
+// import nodemailer from 'nodemailer'
+// import User from '@/models/userModels'
+// import bcryptjs from 'bcryptjs'
 
-export const sendMail = async ({ email, emailType, userId }: any) => {
-  try {
-    // create a hashed token
-    const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+// export const sendMail = async ({ email, emailType, userId }: any) => {
+//   try {
+//     // create a hashed token
+//     const hashedToken = await bcryptjs.hash(userId.toString(), 10)
 
-    if (emailType === 'VERIFY') {
+//     if (emailType === 'VERIFY') {
 
-      await User.findByIdAndUpdate(userId, {
-        verifyToken: hashedToken,
-        verifyTokenExpiry: Date.now() + 3600000
-      })
-    } else if (emailType === "RESET") {
-      await User.findByIdAndUpdate(userId, {
-        forgotPasswordToken: hashedToken,
-        forgotPasswordTokenExpiry: Date.now() + 3600000
-      })
-    }
+//       await User.findByIdAndUpdate(userId, {
+//         verifyToken: hashedToken,
+//         verifyTokenExpiry: Date.now() + 3600000
+//       })
+//     } else if (emailType === "RESET") {
+//       await User.findByIdAndUpdate(userId, {
+//         forgotPasswordToken: hashedToken,
+//         forgotPasswordTokenExpiry: Date.now() + 3600000
+//       })
+//     }
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.USER_PASS,
-      },
-    });
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 465,
+//       secure: true,
+//       auth: {
+//         user: process.env.USER_EMAIL,
+//         pass: process.env.USER_PASS,
+//       },
+//     });
 
-    let mailOptions = {
-      from: process.env.USER_EMAIL,
-      to: email,
-      subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      text: `wellome to my app`,
-      html: `<p>Click<a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}"></a></p>`
-    };
-    const mailResponse = await transporter.sendMail(mailOptions)
-    return mailResponse
+//     let mailOptions = {
+//       from: process.env.USER_EMAIL,
+//       to: email,
+//       subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+//       text: `wellome to my app`,
+//       html: `<p>Click<a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}"></a></p>`
+//     };
+//     const mailResponse = await transporter.sendMail(mailOptions)
+//     return mailResponse
 
-  } catch (error: any) {
-    throw new Error(error.message)
-  }
-}
+//   } catch (error: any) {
+//     throw new Error(error.message)
+//   }
+// }
